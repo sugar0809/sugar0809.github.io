bd3e4e16d024c6c70fb8efe2c3d49460181665cc
sugar0809 <t06420as@gmail.com>
2019-05-06 12:02:56 +0900

    Fix category

--- _plugins/translate_category.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll
-  module TranslateCategory
-    def translate_category(input)
-      @context.registers[:site].config['category_mapping'][input] || input
-    end
-  end
-end
-Liquid::Template.register_filter(Jekyll::TranslateCategory)
